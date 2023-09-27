c3ada613449fb675351e4d9d707598dceac53cc6
w-Andrew <99445523+w-Andrew@users.noreply.github.com>
2023-09-27 08:46:39 -0600

    Creating Repo for Project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//metric <--> imperial conversions
+//Weight, Distance, Tempurature
+//lbs to kg, miles to km, celsius to fahrenheit
+//Must be able to convert single value, as well as array
+
+// mile to km         (val *= 1.60934).toFixed(2);
+//  km to mile        (val *= 0.621371).toFixed(2);
+
+convertTo = () => {
+    
+}

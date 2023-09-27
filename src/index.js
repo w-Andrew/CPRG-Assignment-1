48801e31dc230bd3df378b640b8257788c634a39
w-Andrew <99445523+w-Andrew@users.noreply.github.com>
2023-09-27 09:39:09 -0600

    Skeleton Code and testing
    Added some skeleton codes and did some testing on taking array of ints/floats and converting from miles to km

--- src/index.js
+++ src/index.js
@@ -6,6 +6,84 @@
 // mile to km         (val *= 1.60934).toFixed(2);
 //  km to mile        (val *= 0.621371).toFixed(2);
 
-convertTo = () => {
-    
+//conversion function
+
+var conversionType = 3;
+
+setConversion = (val) => {
+    switch (val) {
+        //lb to kg
+        case 1:
+            conversionType = 1;
+            break;
+        //kg to lb
+        case 2:
+            conversionType = 2;
+            break;
+        //mile to km
+        case 3:
+            conversionType = 3;
+            break;
+        //km to mile
+        case 4:
+            conversionType = 4;
+            break;
+        //c to f
+        case 5:
+            conversionType = 5;
+            break;
+        //f to c
+        case 6:
+            conversionType = 6;
+            break;
+
+    }
+}
+
+convertToArray = (arr) => {
+    //take in user input and split into array of float values
+    let array = arr.split(',').map(Number);
+    console.log(array);
+
+    convertTo(array, conversionType);
+}
+
+convertTo = (array, conversionType) => {
+
+    let newArray = [];
+
+    switch (conversionType) {
+        //lb to kg
+        case 1:
+            conversionType = 1;
+            break;
+        //kg to lb
+        case 2:
+            conversionType = 2;
+            break;
+        //mile to km
+        case 3:
+            console.log(array);
+            for (let num of array) {
+                console.log(num);
+                num = (num * 1.60934).toFixed(2);
+                console.log(num);
+                newArray.push(num);
+            };
+            console.log(newArray);
+            break;
+        //km to mile
+        case 4:
+            conversionType = 4;
+            break;
+        //c to f
+        case 5:
+            conversionType = 5;
+            break;
+        //f to c
+        case 6:
+            conversionType = 6;
+            break;
+
+    }
 }

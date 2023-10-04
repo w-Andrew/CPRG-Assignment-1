084692eb41f460e26366000d263da3b21d76ccfb
w-Andrew <99445523+w-Andrew@users.noreply.github.com>
2023-10-04 15:16:47 -0600

    Finished conversion functions
    Added button, results area, and added some conversion setting functionality to the initial 3 buttons.

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ setConversion = (val) => {
 
 convertToArray = (arr) => {
     //take in user input and split into array of float values
+    console.log(arr);
     let array = arr.split(',').map(Number);
     console.log(array);
 
@@ -55,11 +56,29 @@ convertTo = (array, conversionType) => {
     switch (conversionType) {
         //lb to kg
         case 1:
-            conversionType = 1;
+            console.log(array);
+            for (let num of array) {
+                console.log(num);
+                num = (num * 0.453592).toFixed(2);
+                console.log(num);
+                newArray.push(num);
+            };
+            //set text of results to newArray
+            document.getElementById("results").innerHTML = newArray;
+            console.log(newArray);
             break;
         //kg to lb
         case 2:
-            conversionType = 2;
+            console.log(array);
+            for (let num of array) {
+                console.log(num);
+                num = (num * 2.20462).toFixed(2);
+                console.log(num);
+                newArray.push(num);
+            };
+            //set text of results to newArray
+            document.getElementById("results").innerHTML = newArray;
+            console.log(newArray);
             break;
         //mile to km
         case 3:
@@ -70,19 +89,48 @@ convertTo = (array, conversionType) => {
                 console.log(num);
                 newArray.push(num);
             };
+            //set text of results to newArray
+            document.getElementById("results").innerHTML = newArray;
             console.log(newArray);
             break;
         //km to mile
         case 4:
-            conversionType = 4;
+            console.log(array);
+            for (let num of array) {
+                console.log(num);
+                num = (val *= 0.621371).toFixed(2);
+                console.log(num);
+                newArray.push(num);
+            };
+            //set text of results to newArray
+            document.getElementById("results").innerHTML = newArray;
+            console.log(newArray);
             break;
         //c to f
         case 5:
-            conversionType = 5;
+            console.log(array);
+            for (let num of array) {
+                console.log(num);
+                num = ((num * (9 / 5) + 32)).toFixed(2);
+                console.log(num);
+                newArray.push(num);
+            };
+            //set text of results to newArray
+            document.getElementById("results").innerHTML = newArray;
+            console.log(newArray);
             break;
         //f to c
         case 6:
-            conversionType = 6;
+            console.log(array);
+            for (let num of array) {
+                console.log(num);
+                num = ((num - 32) * (5 / 9)).toFixed(2);
+                console.log(num);
+                newArray.push(num);
+            };
+            //set text of results to newArray
+            document.getElementById("results").innerHTML = newArray;
+            console.log(newArray);
             break;
 
     }

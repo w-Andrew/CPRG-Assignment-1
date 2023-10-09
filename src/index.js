a20d2419a4f9d787855e48a4ba35a9b3e5f739e0
w-Andrew <99445523+w-Andrew@users.noreply.github.com>
2023-10-08 21:21:04 -0600

    Fixed original HTML and Javascript
    Reformatted and did some tailwind to neaten it up.

--- src/index.js
+++ src/index.js
@@ -1,81 +1,21 @@
-// Define the default conversion type
+//metric <--> imperial conversions
+//Weight, Distance, Tempurature
+//lbs to kg, miles to km, celsius to fahrenheit
+//Must be able to convert single value, as well as array
+
+//conversion function
+
 var conversionType = 1;
 
-// Function to update the conversionType and the unitSelector options
-function updateConversionType() {
-  const unitSelector = document.getElementById("unitSelector");
-  const selectedUnit = unitSelector.value;
-
-  // Update the conversionType based on the selected option
-  switch (selectedUnit) {
-    case "lb":
-      conversionType = 1;
-      break;
-    case "kg":
-      conversionType = 2;
-      break;
-    case "miles":
-      conversionType = 3;
-      break;
-    case "km":
-      conversionType = 4;
-      break;
-    case "celsius":
-      conversionType = 5;
-      break;
-    case "fahrenheit":
-      conversionType = 6;
-      break;
-    default:
-      // Handle any other cases or errors here
-      break;
-  }
-
-  // Update the unitSelector options based on the selected conversion type
-  const distanceOptions = unitSelector.querySelectorAll('optgroup[label="Distance"] option');
-  const temperatureOptions = unitSelector.querySelectorAll('optgroup[label="Temperature"] option');
-
-  switch (conversionType) {
-    case 3: // Distance
-      distanceOptions.forEach((option) => {
-        option.disabled = false;
-      });
-      temperatureOptions.forEach((option) => {
-        option.disabled = true;
-      });
-      break;
-    case 5: // Temperature
-      distanceOptions.forEach((option) => {
-        option.disabled = true;
-      });
-      temperatureOptions.forEach((option) => {
-        option.disabled = false;
-      });
-      break;
-    default:
-      // Enable all options for other conversion types
-      distanceOptions.forEach((option) => {
-        option.disabled = false;
-      });
-      temperatureOptions.forEach((option) => {
-        option.disabled = false;
-      });
-      break;
-  }
-}
-
-// Attach the event listener to the select element
-const unitSelector = document.getElementById("unitSelector");
-unitSelector.addEventListener("change", updateConversionType);
-
-// Call the updateConversionType function initially to set the initial conversionType and options
-updateConversionType();
-
-function setConversion(val) {
+setConversion = (val) => {
+  var option1 = document.getElementById("option1");
+  var option2 = document.getElementById("option2");
   switch (val) {
     //lb to kg
     case 1:
       conversionType = 1;
+      option1.innerText = "Lbs";
+      option2.innerText = "Kgs";
       break;
     //kg to lb
     case 2:
@@ -84,6 +24,9 @@ function setConversion(val) {
     //mile to km
     case 3:
       conversionType = 3;
+
+      option1.innerText = "Miles";
+      option2.innerText = "Km";
       break;
     //km to mile
     case 4:
@@ -92,25 +35,49 @@ function setConversion(val) {
     //c to f
     case 5:
       conversionType = 5;
+
+      option1.innerText = "Celsius";
+      option2.innerText = "Fahrenheit";
       break;
     //f to c
     case 6:
       conversionType = 6;
       break;
   }
-}
+};
 
-function convertToArray(arr) {
-  //take in user input and split into an array of float values
+convertToArray = (arr) => {
+  //take in user input and split into array of float values
   console.log(arr);
-  let array = arr.split(',').map(Number);
+  let array = arr.split(",").map(Number);
   console.log(array);
 
   convertTo(array, conversionType);
-}
+};
 
-function convertTo(array, conversionType) {
+reverseConversion = () => {
+  var temp = "";
+  var option1 = document.getElementById("option1");
+  var option2 = document.getElementById("option2");
+  if (conversionType % 2 == 1) {
+    conversionType++;
 
+    temp = option1.innerText;
+    option1.innerText = option2.innerText;
+    option2.innerText = temp;
+
+
+  } else {
+    conversionType--;
+    temp = option1.innerText;
+    option1.innerText = option2.innerText;
+    option2.innerText = temp;
+
+  }
+  document.getElementById("results").innerText = "";
+};
+
+convertTo = (array, conversionType) => {
   let newArray = [];
 
   switch (conversionType) {
@@ -122,7 +89,7 @@ function convertTo(array, conversionType) {
         num = (num * 0.453592).toFixed(2);
         console.log(num);
         newArray.push(num);
-      };
+      }
       //set text of results to newArray
       document.getElementById("results").innerHTML = newArray;
       console.log(newArray);
@@ -135,7 +102,7 @@ function convertTo(array, conversionType) {
         num = (num * 2.20462).toFixed(2);
         console.log(num);
         newArray.push(num);
-      };
+      }
       //set text of results to newArray
       document.getElementById("results").innerHTML = newArray;
       console.log(newArray);
@@ -148,7 +115,7 @@ function convertTo(array, conversionType) {
         num = (num * 1.60934).toFixed(2);
         console.log(num);
         newArray.push(num);
-      };
+      }
       //set text of results to newArray
       document.getElementById("results").innerHTML = newArray;
       console.log(newArray);
@@ -158,10 +125,10 @@ function convertTo(array, conversionType) {
       console.log(array);
       for (let num of array) {
         console.log(num);
-        num = (num * 0.621371).toFixed(2);
+        num = (num *= 0.621371).toFixed(2);
         console.log(num);
         newArray.push(num);
-      };
+      }
       //set text of results to newArray
       document.getElementById("results").innerHTML = newArray;
       console.log(newArray);
@@ -171,10 +138,10 @@ function convertTo(array, conversionType) {
       console.log(array);
       for (let num of array) {
         console.log(num);
-        num = ((num * 9 / 5) + 32).toFixed(2);
+        num = (num * (9 / 5) + 32).toFixed(2);
         console.log(num);
         newArray.push(num);
-      };
+      }
       //set text of results to newArray
       document.getElementById("results").innerHTML = newArray;
       console.log(newArray);
@@ -184,13 +151,13 @@ function convertTo(array, conversionType) {
       console.log(array);
       for (let num of array) {
         console.log(num);
-        num = ((num - 32) * 5 / 9).toFixed(2);
+        num = ((num - 32) * (5 / 9)).toFixed(2);
         console.log(num);
         newArray.push(num);
-      };
+      }
       //set text of results to newArray
       document.getElementById("results").innerHTML = newArray;
       console.log(newArray);
       break;
   }
-}
+};
